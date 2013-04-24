3ae4f7af702cbefe2b5f617b4cdc38f20784b9f0
Kyle Smith <kyle.smithy2@yahoo.com.au>
2013-04-24 20:45:35 +1000

    New Trapdoor update

---
+++ Trapdoor V1.0/boot.py
@@ -0,0 +1,10 @@
+#This is the boot
+import time
+print ("Boot Menu")
+print ("Boot 1.Desktop")
+print ("boot 2.Servers")
+number = input('Enter number: ')
+print("Booting to", number)
+time.sleep(5.5)
+
+
---
+++ Trapdoor V1.0/first time boot.py
@@ -0,0 +1,10 @@
+#This is the first time boot
+import time
+print('hello and welcome to Trapdoor V1.0')
+print("Let's begin what is your name?")
+name = input('Enter your name: ')
+print("Thats a nice name", name)
+print ("now how old are you?")
+age = input('Enter your age: ')
+print ("Ok so you're", age)
+time.sleep(5.5)
