6223122f83c58b7a738dc214d92870fcc4d68f82
Kyle Smith <kyle.smithy2@yahoo.com.au>
2013-04-25 10:41:10 +1000

    Updated the boot.py

--- Trapdoor V1.0/boot.py
+++ Trapdoor V1.0/boot.py
@@ -3,8 +3,8 @@ import time
 print ("Boot Menu")
 print ("Boot 1.Desktop")
 print ("boot 2.Servers")
-number = input('Enter number: ')
-print("Booting to", number)
+Name = input('Enter the name were you would like to boot to: ')
+print("Booting to", Name)
 time.sleep(5.5)
 
 
