0a2a73db82d02dd9a375a2de2dd3bb3d8adffdeb
Kyle Smith <kyle.smithy2@yahoo.com.au>
2013-05-07 20:15:25 +1000

    Update

--- Trapdoor V1.0/Desktop/Games/Alvantia.py
+++ Trapdoor V1.0/Desktop/Games/Alvantia.py
@@ -7,5 +7,11 @@ print("...... One day,", name, "is walking through a forest when all of a sudden
 print("A goblin leaps out of the bush!")
 print("would you like to defend or attack?")
 attack1 = input("Attack or defend?: ")
-print("You", attack1, "the Goblin")
+if attack1==("attack"):
+	print("You attack the goblin and slay it collecting 5 coins!")
+	fob = open('C:/Users/Kyle/Documents/GitHub/Trapdoor/Trapdoor V1.0/Alvantia Game info.txt', 'w')
+	fob.write("You have 5 coins!")
+	fob.close()
+else:
+	print("You defend the goblins attack, and the goblin runs away")
 time.sleep(5.5)
